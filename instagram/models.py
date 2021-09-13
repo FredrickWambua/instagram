c7b25bac7aebc397bb22ca83209dfd03235b9e6a
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-13 16:47:48 +0300

    creates profile models and imports signal

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.signal import post_save
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = CloudinaryField('image')
+    bio = models.TextField(max_length=255)
+
+
+    
+
+    def __str__(self):
+        return self.user.username
+    
+
