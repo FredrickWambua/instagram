056a2a7bc48a1f5ee4262bc91d29916e5c565ea0
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-13 15:23:00 +0300

    creates registration view

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,33 @@
 import instagram
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from .models import *
+from .forms import OrderForm, CreateUserForm
+from .filters import OrderFilter
+
 
 # Create your views here.
 def home(request):
     return render(request, 'instagram/index.html')
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('login')
+			
+
+		context = {'form':form}
+		return render(request, 'accounts/register.html', context)
