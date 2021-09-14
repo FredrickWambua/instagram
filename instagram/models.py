dd423f545f49e4e768a2b06059771f7171e1d0f3
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-14 11:36:17 +0300

    creates the custom models for user

--- instagram/models.py
+++ instagram/models.py
@@ -1,11 +1,30 @@
+from instaclone.settings import TIME_ZONE
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.db.models.fields import DateTimeField
 from cloudinary.models import CloudinaryField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    pass
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=TIME_ZONE.now)
+
+    USERNAME_FIELD = 'email'
+
+    objects = CustomUserManager()
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email= models.EmailField(default=False)
