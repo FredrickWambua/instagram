e034ebd3774a996a30e03f2fe998370ab14c46be
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-14 08:09:33 +0300

    creates update user instancer and receiver decoration

--- instagram/models.py
+++ instagram/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from django.db.models.signal import post_save
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email= models.EmailField(default=False)
     profile_photo = CloudinaryField('image')
     bio = models.TextField(max_length=255)
 
 
-    
-
-    def __str__(self):
-        return self.user.username
-    
-
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
