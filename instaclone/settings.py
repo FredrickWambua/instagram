bc54410ef6c5d1352ffae266e59fae58a8defb0a
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-16 15:29:01 +0300

    commit for deployment

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -56,6 +56,7 @@ AUTH_USER_MODEL='instagram.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,6 +143,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
