85578b65f3f8a75044bc9c1468e1bff6da962aaf
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-18 00:27:12 +0300

    imports Comment class

--- instagram/forms.py
+++ instagram/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db import models
 from django.forms import fields
-from .models import User, Profile, UserManager, Post
+from .models import User, Profile, UserManager, Post, Comment
 
 class signUpForm(UserCreationForm):
     username = forms.CharField(max_length=30, required=False)
