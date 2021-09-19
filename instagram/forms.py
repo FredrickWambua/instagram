fc66bfaa000a377341cae10d35b68a08ff128f9c
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-19 09:23:33 +0300

    creates comment and update profile forms

--- instagram/forms.py
+++ instagram/forms.py
@@ -13,6 +13,18 @@ class signUpForm(UserCreationForm):
         model = User
         fields = ('username','email',)
 
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=255, help_text='Required. Inform a valid email address')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('name', 'profile_photo', 'bio')
+
 class UploadPostForm(forms.ModelForm):
     class Meta:
         model = Post
