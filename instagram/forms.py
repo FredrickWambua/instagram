8be23f6b1618aa737244e739ab90a3ebdf31dc0e
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-20 08:35:07 +0300

    modifies the profile image size

--- instagram/forms.py
+++ instagram/forms.py
@@ -15,7 +15,7 @@ class SignUpForm(UserCreationForm):
         fields = ('username','email', 'password1', 'password2')
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password'])
+        user.set_password(self.cleaned_data['password1'])
         if commit:
             user.save()
         return user
